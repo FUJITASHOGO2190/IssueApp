0623b74ef7e3c5b26c55d81ea7b7d4800ecaea56
FUJITASHOGO2190 <mr.shouuge@gmail.com>
2023-01-22 15:48:31 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/issueapp/IssueEntity.java
@@ -0,0 +1,15 @@
+package in.techcamp.issueapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class IssueEntity {
+    private long id;
+    private String title;
+    private String content;
+    private String period;
+    private String importance;
+
+}
